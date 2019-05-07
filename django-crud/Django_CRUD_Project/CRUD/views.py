b3ca051e83f7355bbe31ca8f067b3a0f4d492d0b
Atif Aslam <aslam43ecehstu@gmail.com>
2019-05-08 04:34:29 +0600

    Update completed

--- django-crud/Django_CRUD_Project/CRUD/views.py
+++ django-crud/Django_CRUD_Project/CRUD/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import BookList
 # Create your views here.
+def index(request):
+    book=BookList.objects.all()
+    return render(request,'index.html',{'book':book})
+
+def edit(request,id):
+    book = BookList.objects.get(id=id)
+    context = {
+    'book': book
+           }
+    return render(request,'edit.html',context)
+
+def delete(request,id):
+    book=BookList.objects.get(id=id)
+    book.delete()
+    return redirect('/')
+
+def update(request,id):
+    book=BookList.objects.get(id=id)
+    book.title=request.GET['title']
+    book.price = request.GET['price']
+    book.author=request.GET['author']
+    book.save()
+    return redirect('/')
+
+def create(request):
+    print(request.POST)
+    title = request.GET['title']
+    price = request.GET['price']
+    author = request.GET['author']
+    book_details=BookList(title=title,price=price,author=author)
+    book_details.save()
+    return redirect('/')
+
+def add_book(request):
+    return render(request,'add_book.html')
